7c5e25c77f02094e910aace9570adb0479b83922
Faroeq <alves_1044@hotmail.com>
2024-06-19 20:00:37 +0200

    Add nativewind

--- App.tsx
+++ App.tsx
@@ -1,21 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Text,
+  View,
+  SafeAreaView,
+} from "react-native";
+
+export type Bar = {
+  title: string;
+  description: string;
+  latitude: number;
+  longitude: number;
+};
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Bar[]>([]);
+
+  const getBars = async () => {
+    try {
+      const response = await fetch(
+        "https://stud.hosted.hr.nl/1041379/barData.json"
+      );
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBars();
+  }, []);
+
+  // show something in a a listview
+
+  const fakeData = [
+    { id: "1", title: "Title Text", description: "Description text" },
+    { id: "2", title: "Text dude", description: "Description text" },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView className="justify-center flex-1 bg-white ">
+      <View>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={fakeData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Text>
+                {item.title}, {item.description}
+              </Text>
+            )}
+          />
+        )}
+      </View>
+      <View>
+        <Text className="text-white">Lil john a milli</Text>
+        <Text className="p-4 mt-4 bg-red-800">Thing</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
