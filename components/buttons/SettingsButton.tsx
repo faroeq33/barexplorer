75f2bbf3a0044c1c54642440f68bc25c2621a6ae
Faroeq <alves_1044@hotmail.com>
2024-06-21 19:18:08 +0200

    Move SettingsButton

--- components/buttons/SettingsButton.tsx
+++ components/buttons/SettingsButton.tsx
@@ -1,5 +1,5 @@
 import { Button } from "react-native";
-import { SettingsScreenProps } from "../navigation/types";
+import { SettingsScreenProps } from "../../navigation/types";
 
 function SettingsButton({ navigation }: SettingsScreenProps) {
   return (
